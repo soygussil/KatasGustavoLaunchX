f29bff75a1b1bd2c207ae520cc6b5b25daab73da
soygussil <gussilv1995@gmail.com>
2022-02-05 21:36:09 -0600

    Agregue modulo 0 y 1 en el nuevo repositorio

---
+++ Modulo-001/calc.py
@@ -0,0 +1,4 @@
+print("Hacer una calculadora de dos numeros en suma")
+numero1=input("primer numero: ")
+numero2=input("segundo numero: ")
+print(int(numero1)+int(numero2))
---
+++ Modulo-001/input.py
@@ -0,0 +1,4 @@
+print("Bienvenido al programa de bienvenida")
+name = input("Introduzca su nombre ")
+print("Saludos: " + name)
+
